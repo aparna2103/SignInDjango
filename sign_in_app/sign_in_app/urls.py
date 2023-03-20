fa874da7b8c72918981080a2bc78c7713ce7d694
Aparna Gupta <agupta53@umd.edu>
2023-03-19 22:49:53 -0400

    Sign in-out project

---
+++ sign_in_app/sign_in_app/urls.py
@@ -0,0 +1,36 @@
+"""sign_in_app URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+from django.urls import path, include
+from django.views.generic import TemplateView
+from django.contrib.auth.views import LogoutView
+
+app_name = "sign_in_app"   
+
+urlpatterns = [
+    path("", include("django.contrib.auth.urls")),
+    path("", views.homepage, name="homepage"),
+    #path("", views.register_request, name="register"),
+    #path('', TemplateView.as_view(template_name="index.html")),
+    path('accounts/', include('allauth.urls')),
+    path('logout', LogoutView.as_view()),
+    path('admin/', admin.site.urls),
+    #path("", include("django.contrib.auth.urls")),
+]
+
